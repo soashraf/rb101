25301ad391073628e98c9508df252338b322a920
Sohail Ashraf <sohail.ashraf@hotmail.com>
2020-12-15 14:35:39 +0000

    rb101, small problems, easy 1, stringy_strings.rb updated to suggested solution

--- small_problems/easy_1/stringy_strings.rb
+++ small_problems/easy_1/stringy_strings.rb
@@ -1,11 +1,23 @@
-def stringy(num)
-  new_string = ''
-  x = 1
-  until x > num
-    new_string << (x % 2).to_s
-    x += 1
+##
+#def stringy(num)
+#  new_string = ''
+#  x = 1
+#  until x > num
+#    new_string << (x % 2).to_s
+#    x += 1
+#  end
+#  new_string
+#end
+
+def stringy(size)
+  numbers = []
+
+  size.times do |index|
+    number = index.even? ? 1 : 0
+    numbers << number
   end
-  new_string
+
+  numbers.join
 end
 
 puts stringy(6) == '101010'
